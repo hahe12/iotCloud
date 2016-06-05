fe75cc9732fe4df75ca646a8c2d38a351e075431
hahe12 <hahe12@sina.com>
2016-06-05 09:47:13 +0800

    login/logout/register finished

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,19 +2,102 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    public function _empty($name){
+        //把所有城市的操作解析到city方法
+         $this->display(T('Index/help'));
+    }
     public function index(){
-        $this->show('hello iotcloud');
+        if (empty($_SESSION['userid']))
+            $this->display(T('Index/login'));
+        else{
+            if ($_SESSION['level'] == 1)
+                $this->show("enter main page with admin");
+            elseif ($_SESSION['level'] == 2)
+                $this->show("enter main page with develop");
+            else 
+                $this->show("enter main page with service");
+        }
+                  
     }
-	public function login($name='testlogin', $passwd='null', $level=3){
-		$this->assign('name',$name);
-		$this->assign('passwd',$passwd);
-		$this->assign('level',$level);
-		$this->display();
+	public function login($username=null, $password=null, $level=3){
+	   if(IS_POST){
+	        if(empty($_POST['username'])) {
+                $this->error('帐号错误！');
+            }elseif (empty($_POST['password'])){
+                $this->error('密码必须！');
+            }
+                        
+            $user = M("user"); // 实例化User对象
+            $condition['username'] = $username;
+            $condition['password'] = $password;
+            $condition['level'] = array('elt',$level);
+            // $condition['level'] = $level;
+            // 把查询条件传入查询方法
+            $cur=$user->where($condition)->select();
+            
+            //var_dump($cur[0]); 
+            
+            if ($cur[0]['username'] == $username and $cur[0]['password'] == $password) {
+                $_SESSION['userid'] =  md5($username);
+                $_SESSION['level'] =  $level;
+                //var_dump($_SESSION['userid'] );
+                //var_dump($_SESSION['level'] ) ;
+                if ($_SESSION['level'] == 1)
+                    $this->show("enter main page with admin");
+                elseif ($_SESSION['level'] == 2)
+                    $this->show("enter main page with develop");
+                elseif ($_SESSION['level'] == 3)
+                    $this->show("enter main page with service");
+            }else{
+                $this->display();
+            }
+        }else{
+            $this->display();
+        }
     }
-	public function register($name='testregister', $passwd='null', $level=3){
-		$this->assign('name',$name);
-		$this->assign('passwd',$passwd);
-		$this->assign('level',$level);
-		$this->display();
+    public function logout(){
+            $_SESSION['userid'] =  null;
+            $this->display(T('Index/login'));
+        }
+	public function register($username='null', $password='null', $email=null, $phone=null, $level=3){
+	    if(IS_POST){
+            if(empty($_POST['username'])) {
+                $this->error('帐号错误！');
+            }elseif (empty($_POST['password'])){
+                $this->error('密码必须！');
+            }elseif (empty($_POST['email'])){
+                $this->error('邮箱必须！');
+            }elseif (empty($_POST['phone'])){
+                $this->error('电话必须！');
+            }elseif (empty($_POST['level'])){
+                $this->error('级别必须！');
+            }
+            
+            $user = M("user"); // 实例化User对象
+            $reg['username'] = $username;
+            $reg['password'] = $password;
+            $reg['email'] = $email;
+            $reg['phone'] = $phone;
+            $reg['level'] = $level;
+            $result= $user->add($reg);
+            
+            if($result){
+                $_SESSION['userid'] =  md5($username);
+                $_SESSION['level'] =  $level;
+            
+                if ($_SESSION['level'] == 1)
+                    $this->show("enter main page with admin");
+                elseif ($_SESSION['level'] == 2)
+                    $this->show("enter main page with develop");
+                elseif ($_SESSION['level'] == 3)
+                    $this->show("enter main page with service");
+            }
+            else{
+                $this->display();
+            }
+        }
+        else{
+            $this->display();
+        }
     }
 }
--- Application/Runtime/Cache/Home/e2f1e8739761dd1b07196da7c9136d50.php
+++ Application/Runtime/Cache/Home/e2f1e8739761dd1b07196da7c9136d50.php
@@ -1,8 +1,67 @@
-<?php if (!defined('THINK_PATH')) exit();?><html>
-<head>
-<title>hello <?php echo ($name); ?></title>
+<?php if (!defined('THINK_PATH')) exit();?>﻿<head>
+<meta http-equiv="Content-Type" content="text/html;" charset=utf-8/>
+<title>用户注册</title>
+<style type="text/css">
+    html{font-size:12px;}
+    fieldset{width:520px; margin: 0 auto;}
+    legend{font-weight:bold; font-size:14px;}
+    label{float:left; width:70px; margin-left:10px;}
+    .left{margin-left:80px;}
+    .input{width:150px;}
+    span{color: #666666;}
+</style>
+<script language=JavaScript>
+<!--
+
+function InputCheck(LoginForm)
+{
+  if (LoginForm.username.value == "")
+  {
+    alert("请输入用户名!");
+    LoginForm.username.focus();
+    return (false);
+  }
+  if (LoginForm.password.value == "")
+  {
+    alert("请输入密码!");
+    LoginForm.password.focus();
+    return (false);
+  }
+}
+
+//-->
+</script>
 </head>
 <body>
-    hello login <?php echo ($name); ?>!
+<div>
+<fieldset>
+<legend>用户注册</legend>
+<form name="LoginForm" method="post" action="/Home/Index/register" onSubmit="return InputCheck(this)">
+<p>
+<label for="username" class="label">用户名:</label>
+<input id="username" name="username" type="text" class="input" />
+<p/>
+<p>
+<label for="password" class="label">密 码:</label>
+<input id="password" name="password" type="password" class="input" />
+<p/>
+<p>
+<label for="email" class="label">邮 箱:</label>
+<input id="email" name="email" type="text" class="input" />
+<p/>
+<p>
+<label for="phone" class="label">电 话:</label>
+<input id="phone" name="phone" type="text" class="input" />
+<p/>
+<p>
+<label for="level" class="label">级别:</label>
+<input id="level" name="level" type="text" class="input" />
+<p/>
+<p>
+<input type="submit" name="submit" value="  注册  " class="left" /> <a href="/Home/Index/login">登陆</a>
+</p>
+</form>
+</fieldset>
+</div>
 </body>
 </html>
